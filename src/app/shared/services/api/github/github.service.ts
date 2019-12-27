aa8c631488b304f6af43bad8388c59250431d48a
Vaibhav Jain <vaibhavjain.ongraph@gmail.com>
2019-12-27 18:33:04 +0530

    setup environment variable

--- src/app/shared/services/api/github/github.service.ts
+++ src/app/shared/services/api/github/github.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  private url = "https://api.github.com/repos/vaibhavjainongraph/FetchGitCommits/commits";
-
   constructor(private httpClient: HttpClient) { }
 
   getCommits() {
-    return this.httpClient.get(this.url);
+    return this.httpClient.get(environment.baseApiUrl + environment.commitApiUrl);
   }
 }
